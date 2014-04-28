5798088803b6304536de9e0f724e9e2ba634efb5
Tom Hickman <thomasdhickman@gmail.com>
2014-04-28 14:26:40 -0400

    Added logo

--- Passbook/gen/com/thickman/passbook/R.java
+++ Passbook/gen/com/thickman/passbook/R.java
@@ -23,29 +23,31 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_new=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int passbook=0x7f020002;
     }
     public static final class id {
-        public static final int action_about=0x7f080012;
-        public static final int action_add=0x7f080013;
-        public static final int action_change_pin=0x7f080014;
-        public static final int btnAdd=0x7f08000a;
+        public static final int action_about=0x7f080013;
+        public static final int action_add=0x7f080014;
+        public static final int action_change_pin=0x7f080015;
+        public static final int btnAdd=0x7f08000b;
         public static final int btnConfirm=0x7f080006;
         public static final int etConfirm=0x7f080005;
-        public static final int etConfirmNew=0x7f08000d;
-        public static final int etKey=0x7f080007;
-        public static final int etNewPin=0x7f08000c;
-        public static final int etOldPin=0x7f08000b;
-        public static final int etPIN=0x7f080004;
-        public static final int etValue1=0x7f080008;
-        public static final int etValue2=0x7f080009;
-        public static final int lvItems=0x7f08000e;
+        public static final int etConfirmNew=0x7f08000e;
+        public static final int etKey=0x7f080008;
+        public static final int etNewPin=0x7f08000d;
+        public static final int etOldPin=0x7f08000c;
+        public static final int etPIN=0x7f080003;
+        public static final int etValue1=0x7f080009;
+        public static final int etValue2=0x7f08000a;
+        public static final int imageView1=0x7f080007;
+        public static final int lvItems=0x7f08000f;
         public static final int scrollView1=0x7f080000;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080001;
-        public static final int txtBanner=0x7f080003;
-        public static final int txtKey=0x7f08000f;
-        public static final int txtValue1=0x7f080010;
-        public static final int txtValue2=0x7f080011;
+        public static final int txtBanner=0x7f080004;
+        public static final int txtKey=0x7f080010;
+        public static final int txtValue1=0x7f080011;
+        public static final int txtValue2=0x7f080012;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
